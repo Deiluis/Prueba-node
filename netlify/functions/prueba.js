5316b98ab49853d387114154dc7d8a794903f5f4
Deiluis <luisconforti04@gmail.com>
2025-09-05 13:40:45 -0300

    Direct fetch test

--- netlify/functions/prueba.js
+++
@@ -1,8 +0,0 @@
-export async function handler(event, context) {
-    const { nombre } = event.queryStringParameters;
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ mensaje: `Proyecto: ${nombre}` })
-    };
-}
--- src/js/router.js
+++ src/js/router.js
@@ -3,7 +3,9 @@ const nombreProyecto = pathParts[2]; // undefined si es solo /proyecto
 
 const obtenerDatos = async (nombreProyecto) => {
 
-    const res = await fetch(`/.netlify/functions/prueba?nombre=${nombreProyecto}`, {
+    // Sanitizar nombre de proyecto
+
+    const res = await fetch(`./productos.json`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json"
